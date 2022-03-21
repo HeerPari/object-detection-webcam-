df154d9dab9d663ad9c74c67087a110cb78e7d3d
HeerPari <87233530+HeerPari@users.noreply.github.com>
2022-03-21 15:36:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+img = "";
+status = "";
+
+
+function preload() {
+    img = loadImage('Dog and cat.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+  }
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#f20580");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#f20580");
+    rect(30, 60, 450, 350);
+
+    fill("#e100ff");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#e100ff");
+    rect(300, 90, 270, 320 );
+}
+
